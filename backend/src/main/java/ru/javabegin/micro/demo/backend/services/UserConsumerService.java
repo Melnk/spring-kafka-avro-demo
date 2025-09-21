39a9391ff7867f83d4572b3a2466705c3626afca
Melnik <melnikoleg080@gmail.com>
2025-09-21 12:21:50 +0300

    Обновлен UserConsumerService до полноценного CRUD-сервиса

--- backend/src/main/java/ru/javabegin/micro/demo/backend/services/UserConsumerService.java
+++ backend/src/main/java/ru/javabegin/micro/demo/backend/services/UserConsumerService.java
@@ -7,16 +7,27 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
 public class UserConsumerService {
+
+    private final Map<String, String> users = new ConcurrentHashMap<>();
 
     @KafkaListener(topics = "users", groupId = "user-service-group")
     public void consume(Object event) {
         if (event instanceof UserCreatedEvent created) {
+            users.put(created.getId(), created.getName());
+            System.out.println("Current users: " + users);
             System.out.println("Received UserCreatedEvent: " + created);
         } else if (event instanceof UserUpdatedEvent updated) {
+            users.put(updated.getId(), updated.getUsername());
+            System.out.println("Current users: " + users);
             System.out.println("Received UserUpdatedEvent: " + updated);
         } else if (event instanceof UserDeletedEvent deleted) {
+            users.remove(deleted.getId());
+            System.out.println("Current users: " + users);
             System.out.println("Deleted User: " + deleted);
         } else {
             System.out.println("Unknown event: " + event);
